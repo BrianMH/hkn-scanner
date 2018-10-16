5b545602273ffda478ce4f4e9053dfa890fc6470
Kenmei <1996brianhnenriquez@gmail.com>
2018-10-16 00:30:07 -0700

    test script done. begin integration

--- testWebDriver.js
+++ testWebDriver.js
@@ -11,7 +11,7 @@ VENMO_PASSWORD = "";
 /*---------------------------------------------------------------------------*/
 
 /*-----------------  SET EARLIEST TRANSACTION DATE --------------------------*/
-const EARLIEST_TRANS_DATE = new Date("December 18, 1995 03:20:10")
+const EARLIEST_TRANS_DATE = new Date("December 18, 2017 03:20:10")
 /*---------------------------------------------------------------------------*/
 
 // Function to execute login for selenium web driver. Returns promise 
@@ -53,17 +53,15 @@ async function waitForURL() {
 
 // Uses the selenium driver to be able to see more of the
 // feed by executing js script upon pressing button.
-function enlargeFeedRequest() {
+async function enlargeFeedRequest() {
 	let button = driver.findElement(webdriver.By.xpath('//button[text()="Load more stories"]'));
 	button.click();
-	let source = driver.sleep(500).then(driver.getPageSource
-							  .then(function(echo) {return echo;}));
-	return source;
+	return driver.sleep(1000);
 }
 
 // Given a page source, use cheerio in order to scrape needed transactions.
 // Returns a list containing the desired list of transactions.
-function attemptParse(pageSource) {
+async function attemptParse(pageSource) {
 	// constant used when modifying date string
 	let DATE_APPEND_STRING = "Public Friends PrivateAmounts are always private.";
 
@@ -71,24 +69,45 @@ function attemptParse(pageSource) {
 	let $ = cheerio.load(pageSource);
 
 	// unroll until earliest desired date
-	transactionList = []
+	let transactionList = []
 	date = Date.now();
 	while(date > EARLIEST_TRANS_DATE) {
-		let lastDate = $(".feed-story-payment").find(".feed-description__notes__meta")[7];
+		let lastDate = $(".feed-story-payment").find(".feed-description__notes__meta");
+		lastDate = lastDate[lastDate.length-1];   // find last date
 		lastDate = new Date($(lastDate).eq(0).text().replace(DATE_APPEND_STRING, ""));
 		console.log(lastDate);
 
 		// enlarge list if possible, otherwise break
 		if($(".feed-more").length == 1) {
-			let newSource = enlargeFeedRequest();
-			$ = newSource;
+			await enlargeFeedRequest();			// wait for expand request to finish
+			let newSource = await driver.getPageSource()	// then grab page source from 'new' page
+			$ = cheerio.load(newSource);
 		} else {
 			break;	// exit to process tx list
 		}
 	}
 
-	// work way through source to collect all desired entries
+	// work way through source to collect all desired entries from final page source
+	$(".feed-story-payment").each(function (i, elem) {
+		// prepare new json entry for addition
+		transactionList.push({})
 
+		// preprocess required entries
+		let tempDate = $(this).find(".feed-description__notes__meta");
+		let tempSwapUsers = $(this).find(".feed-description__notes__headline").text().trim().split(" paid ");
+		let tempExchAmt = parseFloat($(this).find(".feed-description__amount").text().replace("$", ""));
+
+		// log required entires in json
+		transactionList[i].exchangeDate = new Date($(tempDate).eq(0).text().replace(DATE_APPEND_STRING, ""));
+		transactionList[i].exchangeUser1 = tempSwapUsers[0];
+		transactionList[i].exchangeUser2 = tempSwapUsers[1];
+		transactionList[i].exchangeAmount = tempExchAmt;
+
+		// console log outputs
+		console.log(transactionList[i].exchangeDate);
+		console.log(tempSwapUsers);
+		console.log(tempExchAmt);
+	})
 
 	return transactionList;
 }
@@ -112,11 +131,12 @@ async function testScraping() {
 						   .then(waitForURL);
 
 	// try scraping)
-	let results = driver.getPageSource().then(attemptParse);
+	let results = await driver.getPageSource().then(attemptParse);
 	// driver.close();
 	return results;
 }
 
 // execute
-transactionList = testScraping();
-// console.log(transactionList);
+transactionList = testScraping().then(function(toLog) {
+	console.log(toLog);
+});
